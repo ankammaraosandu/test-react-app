81003921d626b897da3f7f38114172551b7452ee
ankammaraosandu <71072063+ankammaraosandu@users.noreply.github.com>
2023-10-03 02:50:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use('/api/v1/auth',require("./routes/userRoutes"))
 app.use('/api/v1/post',require("./routes/postRoutes"))
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        success:true,
+        "msg":"running successful"
+    })
+})
 const PORT = process.env.PORT || 8080
 
 
